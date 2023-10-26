469f204e9b557e2775136265632c1309baf1e66c
Giyoul <116935748+Giyoul@users.noreply.github.com>
2023-10-26 17:45:22 +0900

    [Bronze V] Title: 크냐?, Time: 132 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0) break;
+
+            if(a > b) System.out.println("Yes");
+            else System.out.println("No");
+        }
+    }
+
+}
