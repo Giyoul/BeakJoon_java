56a7f949856a75c10daa4db8157efd62200d8a5b
Giyoul <116935748+Giyoul@users.noreply.github.com>
2023-11-05 00:25:28 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String str =    "         ,r'\"7\n" +
+                        "r`-_   ,'  ,/\n" +
+                        " \\. \". L_r'\n" +
+                        "   `~\\/\n" +
+                        "      |\n" +
+                        "      |\n";
+                        
+        System.out.print(str);
+    }
+}
