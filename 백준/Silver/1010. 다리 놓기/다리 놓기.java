388c27673d2e15ce8ccb2c431c3abe69955f216e
Giyoul <116935748+Giyoul@users.noreply.github.com>
2023-11-13 23:34:01 +0900

    [Silver V] Title: 다리 놓기, Time: 164 ms, Memory: 14492 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] dp = new int[30][30];
+
+        for(int i = 0; i < 30; i++){
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for(int i = 2; i < 30; i++){
+            for(int j = 1; j < 30; j++){
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            System.out.println(dp[M][N]);
+        }
+    }
+}
