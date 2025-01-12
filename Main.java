5ae79a2c562f95d32ffc600f6e0027665c81bd59
Giyoul <cla12093@gmail.com>
2025-01-12 09:21:13 +0900

    :white_check_mark: Update Main

--- Main.java
+++ Main.java
@@ -4,6 +4,5 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     }
 }
