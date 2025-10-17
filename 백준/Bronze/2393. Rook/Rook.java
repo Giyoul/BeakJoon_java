a28dcadea5b105578ac9b2612fb7f8b0b49ac701
김기영 <cla12093@gmail.com>
2025-10-17 14:04:59 +0900

    [Bronze V] Title: Rook, Time: 96 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,20 @@
+
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        StringBuilder sb = new StringBuilder("  ___  ___  ___\n"
+                + "  | |__| |__| |\n"
+                + "  |           |\n"
+                + "   \\_________/\n"
+                + "    \\_______/\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |     |\n"
+                + "     |_____|\n"
+                + "  __/       \\__\n"
+                + " /             \\\n"
+                + "/_______________\\");
+        System.out.println(sb);
+    }
+}
