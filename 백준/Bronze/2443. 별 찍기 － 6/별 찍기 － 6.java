30a074b492d51e2fed3c9e727422029c54419529
김기영 <cla12093@gmail.com>
2025-10-18 14:26:06 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 116 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
