568c1bd7cb90028f8a2f1a63f082fa176727b5a7
김기영 <cla12093@gmail.com>
2025-10-20 10:42:52 +0900

    [Bronze V] Title: 합, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 0) {
+            System.out.println((n + 1) * (n / 2));
+        } else {
+            System.out.println((n + 1) * (n / 2) + (n / 2 + 1));
+        }
+    }
+}
