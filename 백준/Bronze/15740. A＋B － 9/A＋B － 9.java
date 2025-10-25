d8e2bf418f2b7c2323d4c8b9719f5b2af2d72c5f
김기영 <cla12093@gmail.com>
2025-10-25 16:03:13 +0900

    [Bronze V] Title: A+B - 9, Time: 200 ms, Memory: 17420 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger n1 = new BigInteger(st.nextToken());
+        BigInteger n2 = new BigInteger(st.nextToken());
+
+        System.out.println(n1.add(n2));
+    }
+}
