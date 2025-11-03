ad118bd5bb1694ea99bbaa4f6de29d3967945387
김기영 <cla12093@gmail.com>
2025-11-03 09:55:30 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 92 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,13 @@
+
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder("       _.-;;-._\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-;;-._|\n"
+                + "'-..-'|   ||   |\n"
+                + "'-..-'|_.-''-._|");
+
+        System.out.println(sb);
+    }
+}
