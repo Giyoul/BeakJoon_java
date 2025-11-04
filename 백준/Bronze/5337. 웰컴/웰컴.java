c5299480e6821d7f2356083bfe7e3d776e169969
김기영 <cla12093@gmail.com>
2025-11-04 20:45:56 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder(".  .   .\n"
+                + "|  | _ | _. _ ._ _  _\n"
+                + "|/\\|(/.|(_.(_)[ | )(/.");
+
+        System.out.println(sb);
+    }
+}
