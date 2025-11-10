c614f81e2dbef0a6f557258d5657b3002ca7cd95
김기영 <cla12093@gmail.com>
2025-11-10 18:35:46 +0900

    [Bronze V] Title: Hook, Time: 92 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder("#  # #### #### #  #\n"
+                + "#### #  # #  # # #\n"
+                + "#### #  # #  # # #\n"
+                + "#  # #### #### #  #");
+
+        System.out.println(sb);
+    }
+}
