35089bdb338ab6bda74bd666eadcb8dadbd0b36f
김기영 <cla12093@gmail.com>
2025-11-11 16:47:40 +0900

    [Bronze V] Title: We love kriii, Time: 92 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder("강한친구 대한육군\n"
+                + "강한친구 대한육군");
+
+        System.out.println(sb);
+    }
+}
