7c50f9922cb881ab9990e4c67e391ad88695ec9f
김기영 <cla12093@gmail.com>
2025-11-13 14:09:37 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 96 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,8 @@
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder("고려대학교");
+
+        System.out.println(sb);
+    }
+}
