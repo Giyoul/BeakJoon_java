4495cdd1ca8b5256850925600e8d2acb2a63c9b7
김기영 <cla12093@gmail.com>
2025-12-13 13:04:16 +0900

    [Bronze V] Title: Site Score, Time: 112 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        sum += Integer.parseInt(st.nextToken()) * 56;
+        sum += Integer.parseInt(st.nextToken()) * 24;
+        sum += Integer.parseInt(st.nextToken()) * 14;
+        sum += Integer.parseInt(st.nextToken()) * 6;
+
+        System.out.println(sum);
+    }
+}
