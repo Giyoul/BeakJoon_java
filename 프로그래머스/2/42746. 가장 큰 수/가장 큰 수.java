a49a48e1c6584bd2fb47f5ea6304316bb4bf519f
김기영 <cla12093@gmail.com>
2026-05-08 10:17:51 +0900

    [level 2] Title: 가장 큰 수, Time: 91.92 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public String solution(int[] numbers) {
+        List<String> strNumbers = new ArrayList();
+        for(int number : numbers) {
+            strNumbers.add(String.valueOf(number));
+        }
+        strNumbers.sort((o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        
+        if(strNumbers.get(0).equals("0")) return "0";
+        
+        String answer = "";
+        answer = String.join("", strNumbers);
+        
+        return answer;
+    }
+}
